415e4ee013ca07b4f891b33b673910651c83cd5c
tmpic <tomytinez@hotmail.com>
2021-09-20 19:50:59 -0300

    Punto 1 done

--- ruby/lib/Aspects.rb
+++ ruby/lib/Aspects.rb
@@ -1,6 +1,21 @@
 class Aspects
+  class ArgumentError < ::StandardError
+    #def initialize(msg = "wrong number of arguments (0 for +1)")
+    #  super
+    #end
+  end
+
   def on(*args, &bloque)
-    args.each { |argumento| self.funcionMagica(argumento, &bloque)}
+    if args == [] then
+      raise ArgumentError, "wrong number of arguments (0 for +1)"
+    else args.each { |argumento| self.funcionMagica(argumento, &bloque)}
+    #falta contemplar el caso de que si la clase no existe
+    end
+
+  end
+
+  def where()
+    yield
   end
 
   def funcionMagica(algo, &bloque)
@@ -14,38 +29,55 @@ class Aspects
       algo.module_eval(&bloque)
     end
     if(algo.class == Regexp)
-      puts "soy una regex jeje"
+      #if ObjectSpace.each_object.any? {|objeto| funcionMasMagicaAun?(algo, objeto)} then#horrible esto
+        ObjectSpace.each_object(Class) {|clase| if algo.match?(clase.to_s) then clase.class_eval(&bloque) end}
+        ObjectSpace.each_object(Module) {|modulo| if algo.match?(modulo.to_s) then modulo.module_eval(&bloque) end}
+      #else
+      #  raise ArgumentError, msg = "origen vacío? la clase: #{algo} no existe"
+      #end
     end
+  end
+
+  def funcionMasMagicaAun?(algo, objeto)
+    if(objeto.class != NilClass) then algo.match?(objeto.to_s) end
   end
 
 end
 
 #Ejemplo
 module UnModulo
-
 end
 
 class UnaClase
   include UnModulo
 end
 
-unBloque = proc do
+class UnaClase2
+end
+
+name = Proc.new do
+|regex| regex.match
+end
+
+#where = Proc.new do
+#  yield
+#end
+
+unBloque = Proc.new do
   def hola
     puts "hola"
   end
 end
 
-Aspects.new.on UnModulo do
-  def hola
-    puts "hola"
-  end
+def where
+  puts self
+  yield
 end
 
-UnaClase.new.hola
-#puts ObjectSpace.each_object.filter {|objeto| if !objeto.nil? then /^Foo.*/.match?(objeto.to_s.split("<")[1].split(":")[0]) end}
-#puts ObjectSpace.each_object.filter {|objeto|  if !objeto.nil? then (if objeto.to_s == UnaClase.to_s then puts "salmon" end) end}
-#puts ObjectSpace.each_object.filter {|a| a.is_a? UnModulo }#[0].to_s.split("<")[1].split(":")[0]
-puts /^Foo.*/.match?("#<UnaClase:0x0000026970b312e0>")
-#puts UnaClase.to_s
-#puts global_variables
-ObjectSpace.each_object {|a| puts a}
+Aspects.new.on UnaClase do
+  where &unBloque
+end
+
+clasesita = UnaClase.new
+clasesita.send(:hola)
+clasesita.hola
